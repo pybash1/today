cab786c43bbcaa601766a0593680d5464217bb6e
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-12-31 20:56:31 +0530

    refactor: removed boilerplate code

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { tasksRouter } from "~/server/api/routers/tasks";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  tasks: tasksRouter,
 });
 
 // export type definition of API
