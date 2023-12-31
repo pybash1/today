92b933e8dadd11807ed802715251ea7a4cd62797
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-12-31 20:55:57 +0530

    build(env): updated env variables

--- src/env.js
+++ src/env.js
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    DETA_KEY: z.string().min(0),
   },
 
   /**
@@ -25,6 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    DETA_KEY: process.env.DETA_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
